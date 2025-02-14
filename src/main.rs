6b5886dc7083ac9457468bca91b74063c8f0a135
Clarke Kennedy <clarkek0422@gmail.com>
2025-02-14 17:34:41 -0500

    offline multiplayer implemented

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,23 @@ use rand::Rng;
 const TILE_SIZE: f32 = 24.;
 const MARGIN_RATIO: f32 = 16.;
 const GRID_SIZE: i32 = 30;
-const STARTING_POS_X: i32 = 0;
-const STARTING_POS_Y: i32 = 10;
-const STARTING_DIRECTION: Direction = Direction::Right;
-const STARTING_COLOR: (f32, f32, f32) = (0., 1., 1.);
+
+const HOST_STARTING_POSITION_X: i32 = 15;
+const HOST_STARTING_POS_Y: i32 = 10;
+const HOST_STARTING_DIRECTION: Direction = Direction::Right;
+// const HOST_STARTING_COLOR: (f32, f32, f32) = (0., 1., 1.);
+const HOST_COLOR_FN: fn(usize) -> (f32, f32, f32) = |n| -> (f32, f32, f32) {
+    (((f64::cos((n as f64 / 5.) + PI) + 1.) / 2.) as f32, ((f64::cos(n as f64 / 5.) + 1.) / 2.) as f32, 1.)
+};
+
+const GUEST_STARTING_POSITION_X: i32 = 15;
+const GUEST_STARTING_POS_Y: i32 = 20;
+const GUEST_STARTING_DIRECTION: Direction = Direction::Left;
+// const GUEST_STARTING_COLOR: (f32, f32, f32) = (1., 1., 0.);
+const GUEST_COLOR_FN: fn(usize) -> (f32, f32, f32) = |n| -> (f32, f32, f32) {
+    (((f64::cos(n as f64 / 5.) + 1.) / 2.) as f32, 1., ((f64::cos((n as f64 / 5.) + PI) + 1.) / 4.) as f32)
+};
+
 const FRUIT_COLOR: (u8, u8, u8) = (255, 20, 147);
 
 #[derive (Clone, Copy)]
@@ -61,21 +74,22 @@ struct Snake {
     head_pos_x: i32,
     head_pos_y: i32,
     head_facing: Direction,
-    // head_color: (f32, f32, f32),
+    color_fn: fn(usize) -> (f32, f32, f32),
 }
 
 impl Snake {
-    fn new(ctx: &mut Context, pos_x: i32, pos_y: i32, facing: Direction) -> GameResult<Self> {
+    fn new(ctx: &mut Context, head_pos_x: i32, head_pos_y: i32, head_facing: Direction, color_fn: fn(usize) -> (f32, f32, f32)) -> GameResult<Self> {
         Ok(Snake {
-            head_pos_x: pos_x,
-            head_pos_y: pos_y,
-            head_facing: STARTING_DIRECTION,
-            segments: VecDeque::from([Segment::new(ctx, pos_x, pos_y, facing, STARTING_COLOR)?]),
+            head_pos_x,
+            head_pos_y,
+            head_facing,
+            color_fn,
+            segments: VecDeque::from([Segment::new(ctx, head_pos_x, head_pos_y, head_facing, color_fn(0))?]),
         })
     }
 
     // Returning true represents game over due to collision or out of bounds
-    fn push(self: &mut Self, ctx: &mut Context, input_direction: Direction) -> GameResult<bool> {
+    fn push(self: &mut Self, ctx: &mut Context, input_direction: Direction) -> GameResult<> {
         let new_pos_x;
         let new_pos_y;
         match input_direction {
@@ -96,15 +110,16 @@ impl Snake {
                 new_pos_y = self.head_pos_y;
             }
         }
-        if Snake::check_out_of_bounds(new_pos_x, new_pos_y) || self.check_collision(new_pos_x, new_pos_y) {
-            return Ok(true);
-        }
+        
+        // if Snake::check_out_of_bounds(new_pos_x, new_pos_y) || self.check_collision(new_pos_x, new_pos_y) {
+        //     return Ok(true);
+        // }
 
         // let color_difference = (GRID_SIZE as f32).powf(2.);
-        let new_color_r = (f64::cos((self.segments.len() as f64 / 5.) + PI) + 1.) / 2.;
-        let new_color_g = (f64::cos(self.segments.len() as f64 / 5.) + 1.) / 2.;
+        // let new_color_r = (f64::cos((self.segments.len() as f64 / 5.) + PI) + 1.) / 2.;
+        // let new_color_g = (f64::cos(self.segments.len() as f64 / 5.) + 1.) / 2.;
 
-        let new_segment = Segment::new(ctx, new_pos_x, new_pos_y, input_direction, (new_color_r as f32, new_color_g as f32, STARTING_COLOR.2))?;
+        let new_segment = Segment::new(ctx, new_pos_x, new_pos_y, input_direction, (self.color_fn)(self.segments.len()))?;
         self.segments.push_back(new_segment);
 
         // self.head_color = new_color;
@@ -112,7 +127,7 @@ impl Snake {
         self.head_pos_y = new_pos_y;
         self.head_facing = input_direction;
 
-        Ok(false)
+        Ok(())
     }
 
     fn pull(self: &mut Self) {
@@ -122,6 +137,18 @@ impl Snake {
     fn check_collision(self: &Self, pos_x: i32, pos_y: i32) -> bool {
         for segment in self.segments.iter() {
             if pos_x == segment.pos_x && pos_y == segment.pos_y {
+                return true;
+            }
+        }
+        false
+    }
+
+    fn check_self_collision(self: &Self) -> bool {
+        let mut segments_iter = self.segments.iter();
+        segments_iter.next_back();
+
+        for segment in segments_iter {
+            if self.head_pos_x == segment.pos_x && self.head_pos_y == segment.pos_y {
                 return true;
             }
         }
@@ -157,11 +184,11 @@ impl Fruit {
         Ok(Fruit{pos_x, pos_y, mesh})
     }
 
-    fn reposition(self: &mut Self, snake: &Snake) {
+    fn reposition(self: &mut Self, snakes: (&Snake, &Snake)) {
         loop {
             self.pos_x = rand::thread_rng().gen_range(0..=GRID_SIZE-1);
             self.pos_y = rand::thread_rng().gen_range(0..=GRID_SIZE-1);
-            if !snake.check_collision(self.pos_x, self.pos_y) {
+            if !snakes.0.check_collision(self.pos_x, self.pos_y) && !snakes.1.check_collision(self.pos_x, self.pos_y) {
                 break;
             }
         }
@@ -174,24 +201,40 @@ enum GamePhase {
     Over,
 }
 
-struct MainState {
-    phase: GamePhase,
+struct PlayerState {
     input_direction: Direction,
     snake: Snake,
+}
+
+struct MainState {
+    phase: GamePhase,
     fruit: Fruit,
+    host_player: PlayerState,
+    guest_player: PlayerState,
 }
 
 impl MainState {
     fn new(ctx: &mut Context) -> GameResult<MainState> {
 
-        let snake = Snake::new(ctx, STARTING_POS_X, STARTING_POS_Y, STARTING_DIRECTION)?;
+        let host_snake = Snake::new(ctx, HOST_STARTING_POSITION_X, HOST_STARTING_POS_Y, HOST_STARTING_DIRECTION, HOST_COLOR_FN)?;
+        let guest_snake = Snake::new(ctx, GUEST_STARTING_POSITION_X, GUEST_STARTING_POS_Y, GUEST_STARTING_DIRECTION, GUEST_COLOR_FN)?;
+
+        let host_player = PlayerState {
+            input_direction: HOST_STARTING_DIRECTION,
+            snake: host_snake
+        };
+        let guest_player = PlayerState {
+            input_direction: GUEST_STARTING_DIRECTION,
+            snake: guest_snake
+        };
+
 
         Ok(
             MainState {
                 phase: GamePhase::Start,
-                input_direction: STARTING_DIRECTION,
-                snake,
                 fruit: Fruit::new(ctx)?,
+                host_player,
+                guest_player,
             }
         )
     }
@@ -204,15 +247,53 @@ impl event::EventHandler<ggez::GameError> for MainState {
         while ctx.time.check_update_time(TARGET_FPS) {
 
             if let GamePhase::Play = self.phase {
-                if self.snake.push(ctx, self.input_direction)? {
+                // if self.host_player.snake.push(ctx, self.host_player.input_direction)?
+                // || self.guest_player.snake.push(ctx, self.guest_player.input_direction)? {
+                //     self.phase = GamePhase::Over;
+
+                //     self.host_player.input_direction = HOST_STARTING_DIRECTION;
+                //     self.host_player.snake = Snake::new(ctx, HOST_STARTING_POSITION_X, HOST_STARTING_POS_Y, HOST_STARTING_DIRECTION, HOST_COLOR_FN)?;
+                    
+                //     self.guest_player.input_direction = GUEST_STARTING_DIRECTION;
+                //     self.guest_player.snake = Snake::new(ctx, GUEST_STARTING_POSITION_X, GUEST_STARTING_POS_Y, GUEST_STARTING_DIRECTION, HOST_COLOR_FN)?;
+
+                //     self.fruit = Fruit::new(ctx)?;
+                // } else if self.snake.head_pos_x == self.fruit.pos_x && self.snake.head_pos_y == self.fruit.pos_y {
+                //     self.fruit.reposition(&self.snake);
+                // } else {
+                //     self.snake.pull();
+                // }
+
+                self.host_player.snake.push(ctx, self.host_player.input_direction)?;
+                self.guest_player.snake.push(ctx, self.guest_player.input_direction)?;
+
+                if !(self.host_player.snake.head_pos_x == self.fruit.pos_x && self.host_player.snake.head_pos_y == self.fruit.pos_y){
+                    self.host_player.snake.pull();
+                }
+                if !(self.guest_player.snake.head_pos_x == self.fruit.pos_x && self.guest_player.snake.head_pos_y == self.fruit.pos_y){
+                    self.guest_player.snake.pull();
+                }
+
+                if (self.host_player.snake.head_pos_x == self.fruit.pos_x && self.host_player.snake.head_pos_y == self.fruit.pos_y)
+                || (self.guest_player.snake.head_pos_x == self.fruit.pos_x && self.guest_player.snake.head_pos_y == self.fruit.pos_y) {
+                    self.fruit.reposition((&self.host_player.snake, &self.guest_player.snake));
+                }
+
+                if Snake::check_out_of_bounds(self.host_player.snake.head_pos_x, self.host_player.snake.head_pos_y)
+                || Snake::check_out_of_bounds(self.guest_player.snake.head_pos_x, self.guest_player.snake.head_pos_y)
+                || self.host_player.snake.check_self_collision()
+                || self.host_player.snake.check_collision(self.guest_player.snake.head_pos_x, self.guest_player.snake.head_pos_y)
+                || self.guest_player.snake.check_self_collision()
+                || self.guest_player.snake.check_collision(self.host_player.snake.head_pos_x, self.host_player.snake.head_pos_y) {
                     self.phase = GamePhase::Over;
-                    self.input_direction = STARTING_DIRECTION;
-                    self.snake = Snake::new(ctx, STARTING_POS_X, STARTING_POS_Y, STARTING_DIRECTION)?;
+
+                    self.host_player.input_direction = HOST_STARTING_DIRECTION;
+                    self.host_player.snake = Snake::new(ctx, HOST_STARTING_POSITION_X, HOST_STARTING_POS_Y, HOST_STARTING_DIRECTION, HOST_COLOR_FN)?;
+                    
+                    self.guest_player.input_direction = GUEST_STARTING_DIRECTION;
+                    self.guest_player.snake = Snake::new(ctx, GUEST_STARTING_POSITION_X, GUEST_STARTING_POS_Y, GUEST_STARTING_DIRECTION, GUEST_COLOR_FN)?;
+
                     self.fruit = Fruit::new(ctx)?;
-                } else if self.snake.head_pos_x == self.fruit.pos_x && self.snake.head_pos_y == self.fruit.pos_y {
-                    self.fruit.reposition(&self.snake);
-                } else {
-                    self.snake.pull();
                 }
             }
         }
@@ -238,7 +319,7 @@ impl event::EventHandler<ggez::GameError> for MainState {
                 canvas.draw(&text, Vec2::new((GRID_SIZE as f32 / 2.) * TILE_SIZE, (GRID_SIZE as f32 / 2.) * TILE_SIZE))
             },
             GamePhase::Play => {
-                for segment in self.snake.segments.iter() {
+                for segment in self.host_player.snake.segments.iter().chain(self.guest_player.snake.segments.iter()) {
                     let x_offset;
                     let y_offset;
                     match segment.facing {
@@ -289,24 +370,44 @@ impl event::EventHandler<ggez::GameError> for MainState {
         _repeated: bool,
     ) -> GameResult {
         match input.keycode {
-            Some(KeyCode::W) | Some(KeyCode::Up) => {
-                if let Direction::Left | Direction::Right = self.snake.head_facing {
-                    self.input_direction = Direction::Up;
+            Some(KeyCode::W) => {
+                if let Direction::Left | Direction::Right = self.host_player.snake.head_facing {
+                    self.host_player.input_direction = Direction::Up;
                 }
             }
-            Some(KeyCode::A) | Some(KeyCode::Left) => {
-                if let Direction::Up | Direction::Down = self.snake.head_facing {
-                    self.input_direction = Direction::Left;
+            Some(KeyCode::A) => {
+                if let Direction::Up | Direction::Down = self.host_player.snake.head_facing {
+                    self.host_player.input_direction = Direction::Left;
                 }
             }
-            Some(KeyCode::S) | Some(KeyCode::Down) => {
-                if let Direction::Left | Direction::Right = self.snake.head_facing {
-                    self.input_direction = Direction::Down;
+            Some(KeyCode::S) => {
+                if let Direction::Left | Direction::Right = self.host_player.snake.head_facing {
+                    self.host_player.input_direction = Direction::Down;
                 }
             }
-            Some(KeyCode::D) | Some(KeyCode::Right) => {
-                if let Direction::Up | Direction::Down = self.snake.head_facing {
-                    self.input_direction = Direction::Right;
+            Some(KeyCode::D) => {
+                if let Direction::Up | Direction::Down = self.host_player.snake.head_facing {
+                    self.host_player.input_direction = Direction::Right;
+                }
+            }
+            Some(KeyCode::Up) => {
+                if let Direction::Left | Direction::Right = self.guest_player.snake.head_facing {
+                    self.guest_player.input_direction = Direction::Up;
+                }
+            }
+            Some(KeyCode::Left) => {
+                if let Direction::Up | Direction::Down = self.guest_player.snake.head_facing {
+                    self.guest_player.input_direction = Direction::Left;
+                }
+            }
+            Some(KeyCode::Down) => {
+                if let Direction::Left | Direction::Right = self.guest_player.snake.head_facing {
+                    self.guest_player.input_direction = Direction::Down;
+                }
+            }
+            Some(KeyCode::Right) => {
+                if let Direction::Up | Direction::Down = self.guest_player.snake.head_facing {
+                    self.guest_player.input_direction = Direction::Right;
                 }
             }
             Some(KeyCode::Space) => {
